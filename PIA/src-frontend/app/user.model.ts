5b45f8ddbdc57d7a9a754549e299d8d545bc602c
jovinho <44313413+jovinho@users.noreply.github.com>
2019-09-23 17:39:36 +0200

    Changed directory of frontend code.

---
+++ PIA/src-frontend/app/user.model.ts
@@ -0,0 +1,11 @@
+export interface User{
+    fistname: string,
+    lastname: string,
+    username: string,
+    password: string,
+    mail: string,
+    type: string,
+    approved: string
+    status: string,
+    jobs: Array<string>
+}
